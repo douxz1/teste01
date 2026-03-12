572a4439cf5b88b1b1a855cfdaee5f5fcc01643e
douxz1 <thiagodiazgomes@gmail.com>
2026-03-12 12:23:04 -0300

    teste100

---
+++ src/teste01/bbbbbb.java
@@ -0,0 +1,11 @@
+package teste01;
+
+public class bbbbbb {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+
+        int resultado = number1 + number2;
+        System.out.println("Resultado: " + resultado);
+    }
+}
